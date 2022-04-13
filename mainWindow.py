d5d92d21fa2579a3d1e380995a5c63e8229d5915
makshilo <makshilo@mail.ru>
2022-04-14 00:08:47 +0300

    full implementation of generation tab

--- mainWindow.py
+++ mainWindow.py
@@ -7,9 +7,17 @@
 # WARNING! All changes made in this file will be lost!
 
 from PyQt5 import QtCore, QtGui, QtWidgets
-from PyQt5.QtWidgets import QFileDialog
+from PyQt5.QtWidgets import QFileDialog, QMessageBox
+
 from wordForms import *
 from processedWordClass import *
+
+
+def find_checked_radiobutton(radio_buttons):
+    for item in radio_buttons:
+        if item.isChecked():
+            checked_radiobutton_text = item.text()
+            return checked_radiobutton_text
 
 
 class Ui_MainWindow(object):
@@ -53,6 +61,9 @@ class Ui_MainWindow(object):
         self.outputTable.horizontalHeader().setStretchLastSection(True)
         self.outputTable.verticalHeader().setStretchLastSection(False)
         self.verticalLayout_2.addWidget(self.outputTable)
+        self.clearTableButton = QtWidgets.QPushButton(self.processingTab)
+        self.clearTableButton.setObjectName("clearTableButton")
+        self.verticalLayout_2.addWidget(self.clearTableButton)
         self.tabWidget.addTab(self.processingTab, "")
         self.generationTab = QtWidgets.QWidget()
         self.generationTab.setObjectName("generationTab")
@@ -64,42 +75,55 @@ class Ui_MainWindow(object):
         self.horizontalLayout = QtWidgets.QHBoxLayout()
         self.horizontalLayout.setObjectName("horizontalLayout")
         self.prefixGroupBox = QtWidgets.QGroupBox(self.generationTab)
+        self.prefixGroupBox.setEnabled(True)
+        self.prefixGroupBox.setCheckable(False)
         self.prefixGroupBox.setObjectName("prefixGroupBox")
         self.verticalLayout_5 = QtWidgets.QVBoxLayout(self.prefixGroupBox)
         self.verticalLayout_5.setObjectName("verticalLayout_5")
-        self.noPrefButton = QtWidgets.QRadioButton(self.prefixGroupBox)
-        self.noPrefButton.setObjectName("noPrefButton")
-        self.verticalLayout_5.addWidget(self.noPrefButton)
-        self.genPrefButton = QtWidgets.QRadioButton(self.prefixGroupBox)
-        self.genPrefButton.setObjectName("genPrefButton")
-        self.verticalLayout_5.addWidget(self.genPrefButton)
-        self.negPrefButton = QtWidgets.QRadioButton(self.prefixGroupBox)
-        self.negPrefButton.setObjectName("negPrefButton")
-        self.verticalLayout_5.addWidget(self.negPrefButton)
+        self.noPrefRadio = QtWidgets.QRadioButton(self.prefixGroupBox)
+        self.noPrefRadio.setChecked(True)
+        self.noPrefRadio.setObjectName("noPrefRadio")
+        self.verticalLayout_5.addWidget(self.noPrefRadio)
+        self.genPrefRadio = QtWidgets.QRadioButton(self.prefixGroupBox)
+        self.genPrefRadio.setObjectName("genPrefRadio")
+        self.verticalLayout_5.addWidget(self.genPrefRadio)
+        self.negPrefRadio = QtWidgets.QRadioButton(self.prefixGroupBox)
+        self.negPrefRadio.setObjectName("negPrefRadio")
+        self.verticalLayout_5.addWidget(self.negPrefRadio)
         self.horizontalLayout.addWidget(self.prefixGroupBox)
         self.suffixGroupBox = QtWidgets.QGroupBox(self.generationTab)
         self.suffixGroupBox.setObjectName("suffixGroupBox")
         self.verticalLayout_4 = QtWidgets.QVBoxLayout(self.suffixGroupBox)
         self.verticalLayout_4.setObjectName("verticalLayout_4")
-        self.nounButton = QtWidgets.QRadioButton(self.suffixGroupBox)
-        self.nounButton.setObjectName("nounButton")
-        self.verticalLayout_4.addWidget(self.nounButton)
-        self.adjectiveButton = QtWidgets.QRadioButton(self.suffixGroupBox)
-        self.adjectiveButton.setObjectName("adjectiveButton")
-        self.verticalLayout_4.addWidget(self.adjectiveButton)
-        self.verbButton = QtWidgets.QRadioButton(self.suffixGroupBox)
-        self.verbButton.setObjectName("verbButton")
-        self.verticalLayout_4.addWidget(self.verbButton)
-        self.adverbButton = QtWidgets.QRadioButton(self.suffixGroupBox)
-        self.adverbButton.setObjectName("adverbButton")
-        self.verticalLayout_4.addWidget(self.adverbButton)
+        self.noSuffixRadio = QtWidgets.QRadioButton(self.suffixGroupBox)
+        self.noSuffixRadio.setChecked(True)
+        self.noSuffixRadio.setObjectName("noSuffixRadio")
+        self.verticalLayout_4.addWidget(self.noSuffixRadio)
+        self.nounRadio = QtWidgets.QRadioButton(self.suffixGroupBox)
+        self.nounRadio.setObjectName("nounRadio")
+        self.verticalLayout_4.addWidget(self.nounRadio)
+        self.adjectiveRadio = QtWidgets.QRadioButton(self.suffixGroupBox)
+        self.adjectiveRadio.setObjectName("adjectiveRadio")
+        self.verticalLayout_4.addWidget(self.adjectiveRadio)
+        self.verbRadio = QtWidgets.QRadioButton(self.suffixGroupBox)
+        self.verbRadio.setObjectName("verbRadio")
+        self.verticalLayout_4.addWidget(self.verbRadio)
+        self.adverbRadio = QtWidgets.QRadioButton(self.suffixGroupBox)
+        self.adverbRadio.setObjectName("adverbRadio")
+        self.verticalLayout_4.addWidget(self.adverbRadio)
         self.horizontalLayout.addWidget(self.suffixGroupBox)
         self.verticalLayout_3.addLayout(self.horizontalLayout)
         self.horizontalLayout_2 = QtWidgets.QHBoxLayout()
         self.horizontalLayout_2.setObjectName("horizontalLayout_2")
+        self.verticalLayout_7 = QtWidgets.QVBoxLayout()
+        self.verticalLayout_7.setObjectName("verticalLayout_7")
         self.generateButton = QtWidgets.QPushButton(self.generationTab)
         self.generateButton.setObjectName("generateButton")
-        self.horizontalLayout_2.addWidget(self.generateButton)
+        self.verticalLayout_7.addWidget(self.generateButton)
+        self.clearOutputButton = QtWidgets.QPushButton(self.generationTab)
+        self.clearOutputButton.setObjectName("clearOutputButton")
+        self.verticalLayout_7.addWidget(self.clearOutputButton)
+        self.horizontalLayout_2.addLayout(self.verticalLayout_7)
         self.generationOutputTextBrowser = QtWidgets.QTextBrowser(self.generationTab)
         self.generationOutputTextBrowser.setObjectName("generationOutputTextBrowser")
         self.horizontalLayout_2.addWidget(self.generationOutputTextBrowser)
@@ -121,14 +145,21 @@ class Ui_MainWindow(object):
         QtCore.QMetaObject.connectSlotsByName(MainWindow)
 
         self.readFileButton.clicked.connect(self.open_file)
-        self.textProcessButton.clicked.connect(self.process_text)
+        self.textProcessButton.clicked.connect(self.text_process_button_clicked)
+        self.generateButton.clicked.connect(self.generate_button_clicked)
+        self.clearTableButton.clicked.connect(self.clear_table_button_clicked)
+        self.clearOutputButton.clicked.connect(self.clear_output_button_clicked)
 
     def open_file(self):
-        fileName, _ = QFileDialog.getOpenFileName(None, 'Open file', '', 'Text files (*.txt)')
-        with open(fileName) as f:
+        file_name, _ = QFileDialog.getOpenFileName(None, 'Open file', '', 'Text files (*.txt)')
+        with open(file_name) as f:
             self.inputTextEdit.setText(f.read())
 
-    def process_text(self):
+    def text_process_button_clicked(self):
+        # check if the input is empty
+        if self.inputTextEdit.toPlainText() == '':
+            QMessageBox.warning(None, 'Warning', 'Please input text first!')
+            return
         text = self.inputTextEdit.toPlainText()
         text = text_preprocessing(text)
         text = add_pos_tag(text)
@@ -139,9 +170,12 @@ class Ui_MainWindow(object):
         processed_words = []
         for word in text:
             processed_words.append(ProcessedWord(word, get_pos_tag(word)[0][1],
-                                                 get_prefixes(word, general_prefixes) + get_prefixes(word, negative_prefixes),
-                                                 get_suffixes(word, noun_suffixes) + get_suffixes(word, adjective_suffixes) +
-                                                 get_suffixes(word, verb_suffixes) + get_suffixes(word, adverb_suffixes)))
+                                                 get_prefixes(word, general_prefixes) + get_prefixes(word,
+                                                                                                     negative_prefixes),
+                                                 get_suffixes(word, noun_suffixes) + get_suffixes(word,
+                                                                                                  adjective_suffixes) +
+                                                 get_suffixes(word, verb_suffixes) + get_suffixes(word,
+                                                                                                  adverb_suffixes)))
 
         # fill outputTable with processed words
         self.outputTable.setRowCount(len(processed_words))
@@ -151,6 +185,50 @@ class Ui_MainWindow(object):
             self.outputTable.setItem(i, 2, QtWidgets.QTableWidgetItem(str(word.correct_prefixes)))
             self.outputTable.setItem(i, 3, QtWidgets.QTableWidgetItem(str(word.correct_suffixes)))
 
+    def generate_button_clicked(self):
+        # check if input line is empty
+        if self.generationInputLine.text() == '':
+            QMessageBox.warning(None, 'Warning', 'Input line is empty!')
+            return
+
+        word = self.generationInputLine.text()
+        word = text_preprocessing(word)
+        word = add_pos_tag(word)
+        word = lemmatize_text(word)
+
+        if word == []:
+            QMessageBox.warning(None, 'Warning', 'Word is not valid!')
+            return
+
+        prefix_param = {
+            'no prefix': ' ',
+            'general prefix': general_prefixes,
+            'negative prefix': negative_prefixes
+        }
+
+        suffix_param = {
+            'no suffix': ' ',
+            'noun': noun_suffixes,
+            'adjective': adjective_suffixes,
+            'verb': verb_suffixes,
+            'adverb': adverb_suffixes
+        }
+
+        prefix_group = prefix_param[find_checked_radiobutton(self.prefixGroupBox.findChildren(QtWidgets.QRadioButton))]
+        suffix_group = suffix_param[find_checked_radiobutton(self.suffixGroupBox.findChildren(QtWidgets.QRadioButton))]
+
+        generated_words = generate_possible_words(word.pop(0), prefix_group, suffix_group)
+
+        generated_words = [word.strip() for word in generated_words]
+
+        for word in generated_words:
+            self.generationOutputTextBrowser.append(word)
+
+    def clear_table_button_clicked(self):
+        self.outputTable.setRowCount(0)
+
+    def clear_output_button_clicked(self):
+        self.generationOutputTextBrowser.clear()
 
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
@@ -165,32 +243,39 @@ class Ui_MainWindow(object):
         item.setText(_translate("MainWindow", "Prefixes"))
         item = self.outputTable.horizontalHeaderItem(3)
         item.setText(_translate("MainWindow", "Suffixes"))
-        self.tabWidget.setTabText(self.tabWidget.indexOf(self.processingTab), _translate("MainWindow", "Processing"))
+        self.clearTableButton.setText(_translate("MainWindow", "Clear table"))
+        self.tabWidget.setTabText(self.tabWidget.indexOf(self.processingTab),
+                                  _translate("MainWindow", "Processing"))
         self.prefixGroupBox.setTitle(_translate("MainWindow", "Prefix"))
-        self.noPrefButton.setText(_translate("MainWindow", "no prefix"))
-        self.genPrefButton.setText(_translate("MainWindow", "general prefix"))
-        self.negPrefButton.setText(_translate("MainWindow", "negative prefix"))
+        self.noPrefRadio.setText(_translate("MainWindow", "no prefix"))
+        self.genPrefRadio.setText(_translate("MainWindow", "general prefix"))
+        self.negPrefRadio.setText(_translate("MainWindow", "negative prefix"))
         self.suffixGroupBox.setTitle(_translate("MainWindow", "Suffix"))
-        self.nounButton.setText(_translate("MainWindow", "noun"))
-        self.adjectiveButton.setText(_translate("MainWindow", "adjective"))
-        self.verbButton.setText(_translate("MainWindow", "verb"))
-        self.adverbButton.setText(_translate("MainWindow", "adverb"))
+        self.noSuffixRadio.setText(_translate("MainWindow", "no suffix"))
+        self.nounRadio.setText(_translate("MainWindow", "noun"))
+        self.adjectiveRadio.setText(_translate("MainWindow", "adjective"))
+        self.verbRadio.setText(_translate("MainWindow", "verb"))
+        self.adverbRadio.setText(_translate("MainWindow", "adverb"))
         self.generateButton.setText(_translate("MainWindow", "Generate"))
-        self.tabWidget.setTabText(self.tabWidget.indexOf(self.generationTab), _translate("MainWindow", "Generation"))
-        self.helpTextBrowser.setHtml(_translate("MainWindow", "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
-"<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\n"
-"p, li { white-space: pre-wrap; }\n"
-"</style></head><body style=\" font-family:\'Fira Sans Semi-Light\'; font-size:10pt; font-weight:400; font-style:normal;\">\n"
-"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\">This small program is aimed to process text and/or generate some wordforms</p>\n"
-"<p style=\"-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><br /></p>\n"
-"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\">Processing tab is capable of processing text which you can read from file or directly input in the textbox. After pressing process button program will output a table of all recognized wordforms of which we can find at the our given vocabulary  with their lemmas and possible morphemas</p>\n"
-"<p style=\"-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><br /></p>\n"
-"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\">Generation tab is meant to generate wordforms using parameters also present in this tab. All you need is to enter a desired word in the text line set some wanted parameters and pres generate button.</p></body></html>"))
+        self.clearOutputButton.setText(_translate("MainWindow", "Clear output"))
+        self.tabWidget.setTabText(self.tabWidget.indexOf(self.generationTab),
+                                  _translate("MainWindow", "Generation"))
+        self.helpTextBrowser.setHtml(_translate("MainWindow",
+                                                "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
+                                                "<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\n"
+                                                "p, li { white-space: pre-wrap; }\n"
+                                                "</style></head><body style=\" font-family:\'Fira Sans Semi-Light\'; font-size:10pt; font-weight:400; font-style:normal;\">\n"
+                                                "<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\">This small program is aimed to process text and/or generate some wordforms</p>\n"
+                                                "<p style=\"-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><br /></p>\n"
+                                                "<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\">Processing tab is capable of processing text which you can read from file or directly input in the textbox. After pressing process button program will output a table of all recognized wordforms of which we can find at the our given vocabulary  with their lemmas and possible morphemas</p>\n"
+                                                "<p style=\"-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><br /></p>\n"
+                                                "<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\">Generation tab is meant to generate wordforms using parameters also present in this tab. All you need is to enter a desired word in the text line set some wanted parameters and pres generate button.</p></body></html>"))
         self.tabWidget.setTabText(self.tabWidget.indexOf(self.helpTab), _translate("MainWindow", "Help"))
 
 
 if __name__ == "__main__":
     import sys
+
     app = QtWidgets.QApplication(sys.argv)
     MainWindow = QtWidgets.QMainWindow()
     ui = Ui_MainWindow()
